a0af6b593a16efa981157075b910f2f28017f068
Jack Killelea <JackDKillelea@users.noreply.github.com>
2018-08-20 22:46:13 +0100

    VR Platform Experience
    This is a complete VR Experience, you click your mouse to move from place to place. You can put on a VR headset and look round as you are travelling around the basic terrain. This was created to showcase VR developement.

---
+++ Assets/Assets/MyTools/Scripts/CameraDraggingVR.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JacksScripts
+{
+
+    public class CameraDraggingVR : MonoBehaviour
+    {
+#if UNITY_EDITOR
+
+        bool isDragging = false; // flag to keep track of draging
+        float startPosX; // mouse start position X
+        float startPosY; // mouse start position Y
+
+        Camera cam; // camera component
+
+        // Use this for initialization
+        void Start()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if right click is pressed then do nothing but if dragged at the same time do something
+            if (Input.GetMouseButtonDown(1) && !isDragging)
+            {
+                isDragging = true;
+                startPosX = Input.mousePosition.x; // initial mouse point X
+                startPosY = Input.mousePosition.y; // initial mouse point Y
+            }
+            else if (Input.GetMouseButtonUp(1) && isDragging) // if the right button is not pressed but we are dragging
+            {
+                isDragging = false;
+            }
+        }
+
+        void LateUpdate()
+        {
+            if (isDragging)
+            {   // get the end position of where the mouse is
+                float endPosX = Input.mousePosition.x;
+                float endPosY = Input.mousePosition.y;
+
+                // get the difference by the two positions
+                float difX = endPosX - startPosX;
+                float difY = endPosY - startPosY;
+
+                // get the new screen centre
+                float newCentreX = Screen.width / 2 + difX;
+                float newCentreY = Screen.height / 2 + difY;
+
+                // get the world coord
+                Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(newCentreX, newCentreY, cam.nearClipPlane));
+
+                // makes the camera look at the worldPos
+                transform.LookAt(worldPos);
+
+                // starting pos for next call so it doesnt use the original start every time
+                startPosX = endPosX;
+                startPosY = endPosY;
+            }
+        }
+#endif
+    }
+}
---
+++ Assets/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+    public float speed = 1;     // speed of the platform
+    bool isMoving = false;      // change depending on if the user pressed the right key
+
+    public Transform[] targets; // array of transforms for the platform to move to
+
+    private int nextDest;
+	// Use this for initialization
+	void Start () { 
+        transform.position = targets[0].position;
+
+        nextDest = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInput();
+        Movement();
+	}
+
+    // checks for user input, if correct key is pressed handle movement by checking the Fire1 input
+    private void HandleInput()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            isMoving = !isMoving; // set isMoving to its inverse
+        }
+    }
+
+    // user input with movement
+    void Movement()
+    {
+        // if Fire1 input is false then return
+        if(!isMoving)
+        {
+            return;
+        }
+
+        // calculate the distance from the current target
+        float distance = Vector3.Distance(transform.position, targets[nextDest].position);
+
+        // Has it arrived, if not caculate the step between the two points and move there
+        if (distance > 0)
+        {
+            float step = speed * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, targets[nextDest].position, step);
+        } else
+        {
+            nextDest++; // increase the index by 1 when you get to the destination
+
+            // if you are outside of the array length go back to 0
+            if(nextDest == targets.Length)
+            {
+                nextDest = 0;
+            }
+            isMoving = false;
+        }
+    }
+}
+
